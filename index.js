cc30687491a207c9bb81f18f745c089b61c458ec
chaorace@gmail.com <chaorace@gmail.com>
2020-02-24 10:55:06 -0500

    Renamed --input to --includes
    I'm moving forward with goal-specific options, instead of trying to
    divine different task types. I also added a console log output to the
    build task

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import yargs from "yargs";
 import rollup from "rollup";
 import rollupIncludesConfig from "./config/rollup-includes.js";
 
+yargs.array('includes')
 const argv = yargs.argv;
 
-const buildBundle = async (inputFile, rollupOptions) => {
+const buildBundle = (rollupOptions) => async (inputFile) => {
     const options = await rollupOptions(inputFile);
     const bundle = await rollup.rollup(options.input);
     return bundle.write(options.output);
 };
 
-buildBundle(argv.input, rollupIncludesConfig);
+const logOutputPath = async (rollupResultP) =>
+      console.log(`Built file: ${(await rollupResultP).output[0].fileName}`)
+
+argv.includes
+    .map(buildBundle(rollupIncludesConfig))
+    .map(logOutputPath)
