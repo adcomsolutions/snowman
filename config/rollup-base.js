d1cc5939b3fad63d7513668b934c950cbca5dca6
chaorace@gmail.com <chaorace@gmail.com>
2020-02-27 12:44:18 -0500

    Removed Rhino polyfill injection
    As it turns out, Scoped apps can't modify builtin prototypes, so we're
    sol there. Removed the polyfill since it only ended up causing errors.

--- config/rollup-base.js
+++ config/rollup-base.js
@@ -1,18 +1,14 @@
-import { getConfig } from '../src/config-helper.js';
 import {
     baseify,
     getOutputFilePath,
     stripAppName,
 } from '../src/rollup-helper.js';
 
-import fs from 'fs-extra';
 import rollupBabel from 'rollup-plugin-babel';
 import rollupBabelConfig from './babel-rollup.js';
 import rollupAlias from '@rollup/plugin-alias';
 import { mainAliasConfig } from './alias-rollup.js';
 
-const config = getConfig();
-const bundleIntroP = fs.readFile(config.rhinoPolyfillPath, 'utf8');
 const bundleBanner = `// Rollup file built on ${new Date().toGMTString()}`;
 
 export default async (inputFile) => {
@@ -30,7 +26,6 @@ export default async (inputFile) => {
             name: stripAppName(inputBase),
             file: getOutputFilePath(inputFile),
             banner: bundleBanner,
-            intro: await bundleIntroP,
             format: 'iife',
             strict: false,
             interop: false,
