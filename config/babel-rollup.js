69c4fc2f419082ef74dea8a825af067cedd555be
chaorace@gmail.com <chaorace@gmail.com>
2020-03-01 14:57:14 -0500

    Fix issue with Babel plugins when used as package
    Babel's plugin resolver broke when I set this up as a package, so I had
    to switch to manually resolving plugin paths instead

--- config/babel-rollup.js
+++ config/babel-rollup.js
@@ -1,28 +1,39 @@
+// Babel's resolver gets finnicky when the using it as a dependency of a package (possibly only an ES6 module issue?)
+// We work around this by resolving plugin paths ourselves using node require scoped to this package
+// HACK: This means you can't use the shorter babel import syntax that omits "plugin-"
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 export default {
     exclude: 'node_modules/**',
+    babelrc: false,
     plugins: [
         ['@babel/plugin-transform-for-of', { loose: true }],
-        ['@babel/proposal-nullish-coalescing-operator', { loose: true }],
-        ['@babel/proposal-optional-catch-binding'],
-        ['@babel/proposal-optional-chaining', { loose: true }],
-        ['@babel/transform-member-expression-literals'],
-        ['@babel/transform-property-literals'],
-        ['@babel/transform-arrow-functions'],
-        ['@babel/transform-block-scoped-functions'],
-        ['@babel/transform-block-scoping'],
-        ['@babel/proposal-class-properties', { loose: true }],
+        ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: true }],
+        ['@babel/plugin-proposal-optional-catch-binding'],
+        ['@babel/plugin-proposal-optional-chaining', { loose: true }],
+        ['@babel/plugin-transform-member-expression-literals'],
+        ['@babel/plugin-transform-property-literals'],
+        ['@babel/plugin-transform-arrow-functions'],
+        ['@babel/plugin-transform-block-scoped-functions'],
+        ['@babel/plugin-transform-block-scoping'],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
         ['@babel/plugin-transform-classes', { loose: true }],
-        ['@babel/transform-computed-properties', { loose: true }],
-        ['@babel/transform-destructuring', { loose: true, useBuiltIns: true }],
-        ['@babel/transform-literals'],
-        ['@babel/transform-parameters', { loose: true }],
-        ['@babel/transform-shorthand-properties'],
-        ['@babel/transform-spread', { loose: true }],
+        ['@babel/plugin-transform-computed-properties', { loose: true }],
+        [
+            '@babel/plugin-transform-destructuring',
+            { loose: true, useBuiltIns: true },
+        ],
+        ['@babel/plugin-transform-literals'],
+        ['@babel/plugin-transform-parameters', { loose: true }],
+        ['@babel/plugin-transform-shorthand-properties'],
+        ['@babel/plugin-transform-spread', { loose: true }],
         [
             '@babel/plugin-proposal-object-rest-spread',
             { loose: true, useBuiltIns: true },
         ],
-        ['@babel/transform-template-literals', { loose: true }],
-        ['@babel/transform-exponentiation-operator'],
-    ],
+        ['@babel/plugin-transform-template-literals', { loose: true }],
+        ['@babel/plugin-transform-exponentiation-operator'],
+    ].map(([pluginPath, options]) => [require.resolve(pluginPath), options]),
 };
