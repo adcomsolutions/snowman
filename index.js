dd2d833ce5edbd2690cb5ee10d3ee9b03fd9f77d
chaorace@gmail.com <chaorace@gmail.com>
2020-05-19 15:07:26 -0400

    Removed --includes option
    In order to make sure we have strong parity with the vscode extension
    file structure, we will need to stop handling script includes as a special case.

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
 import rollup from 'rollup';
-import rollupIncludesConfig from './config/rollup-includes.js';
 import rollupBackgroundConfig from './config/rollup-background.js';
 import { postProcessOutput } from './src/build-helper.js';
 import { invertFn, testNullish } from './src/utils.js';
 import { resolve } from 'path';
 
-yargs.array('includes');
-yargs.default('includes', []);
-yargs.alias('inc', 'includes');
 yargs.array('background');
 yargs.default('background', []);
 yargs.alias('bg', 'background');
 const argv = yargs.argv;
 
 const resolveLocalFile = (_) => resolve(process.cwd(), _);
-const includesSrcFiles = argv.includes.map(resolveLocalFile);
 const backgroundSrcFiles = argv.background.map(resolveLocalFile);
 
 const buildBundle = (rollupOptions) => async (inputFile) => {
@@ -34,13 +29,10 @@ const logBuiltFile = async (rollupResultP) => {
     console.log(output[0].fileName);
 };
 
-const includesBuildP = includesSrcFiles.map(buildBundle(rollupIncludesConfig));
 const backgroundBuildP = backgroundSrcFiles.map(
     buildBundle(rollupBackgroundConfig)
 );
 
-const allBuilds = [includesBuildP, backgroundBuildP].filter(
-    invertFn(testNullish)
-);
+const allBuilds = [backgroundBuildP].filter(invertFn(testNullish));
 
 allBuilds.map((buildGroup) => buildGroup.map(logBuiltFile));
