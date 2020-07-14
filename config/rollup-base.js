a65e229b1fa6f76f1f6081eb4b462c9eb487f96a
chaorace@gmail.com <chaorace@gmail.com>
2020-07-14 19:01:56 -0400

    Upgraded Rollup babel plugin

--- config/rollup-base.js
+++ config/rollup-base.js
@@ -3,7 +3,7 @@ import { getOutputFilePath } from '../src/vs-helper.js';
 import rollupSynthetic from './rollup-enable-synthetic-imports.js';
 import rollupExportFix from './rollup-named-export-to-default.js';
 
-import rollupBabel from 'rollup-plugin-babel';
+import rollupBabel from '@rollup/plugin-babel';
 import rollupBabelConfig from './babel-rollup.js';
 import rollupAlias from '@rollup/plugin-alias';
 import { mainAliasConfig } from './alias-rollup.js';
@@ -18,7 +18,7 @@ export default (inputFile) => {
         input: {
             input: inputFile,
             plugins: [
-                rollupBabel(rollupBabelConfig),
+                rollupBabel.babel(rollupBabelConfig),
                 rollupAlias(mainAliasConfig(inputFile)),
                 rollupSynthetic,
                 rollupExportFix,
