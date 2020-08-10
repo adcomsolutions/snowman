ec1259b23999b770d8e20507cd5835185d1f4322
chaorace@gmail.com <chaorace@gmail.com>
2020-08-10 12:49:55 -0400

    Fix build failure case for default export files

--- config/rollup-enable-synthetic-imports.js
+++ config/rollup-enable-synthetic-imports.js
@@ -1,6 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
 export default {
     // Resolve imports normally, but inject syntheticNamedExports into the options if not external
     resolveId(id, importer) {
+        // Catching bad file names is broken with syntheticNamedImports
+        // We manually handle these situations to ensure we fail with a descriptive error message
+        const idPath = importer ? path.resolve(path.dirname(importer), id) : id;
+        // Manually resolve relative imports based on the importer before verifying the filepath
+        if (!fs.existsSync(idPath))
+            this.error(
+                `${importer} attempted to import nonexistent file: ${id}`
+            );
+
         const normalResolutionP = this.resolve(id, importer, {
             skipSelf: true,
         });
